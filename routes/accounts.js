98f00feda5ee22e6f75a7d8a579bb48c4dc85acb
Albert Sare <thesundaymailman@gmail.com>
2018-08-21 13:06:01 -0700

    working on routes to update and/or add new bills

--- routes/accounts.js
+++ routes/accounts.js
@@ -40,10 +40,70 @@ router.post('/', (req, res, next) => {
     .catch(err => next(err));
 });
 
-// ========== PUT (update) a bill, or add a bill =====
-router.put('/', (req, res, next) => {
-  const userId = req.user.id;
+// ========== POST (create) a new bill within an account
+router.post('/:id', (req, res, next) => {
+  // grab accountId, userId, and newBill data
+  const accountId = req.params.id;
   const {frequency, dueDate, amount} = req.body;
+  // construct newBill
+  const newBill = {
+    frequency,
+    dueDate,
+    amount,
+  };
+
+  return Account.findById(accountId)
+    .then(result => {
+      const account = result;
+      account.bills = [...account.bills, newBill];
+      account.save()
+        .then(account => res.status(201).json(account));
+    })
+    .catch(err => next(err));
+});
+
+// ========== PUT (update) an exsiting bill within an account =====
+router.put('/:id', (req, res, next) => {
+  // grab accountId, userId, and updateBill data
+  // should bill be referenced by index? or by _id?
+  const userId = req.user.id;
+  const accountId = req.params.id;
+  const {frequency, dueDate, amount, isPaid} = req.body;
+  // construct updateBill
+  const updateBill = {
+    frequency,
+    dueDate,
+    amount,
+    isPaid
+  };
+
+  // account.find(account.bills._id: blah)
 });
 
 module.exports = {router};
+
+
+
+/*
+user's account
+
+[
+    {
+        "url": "www.netflix.com",
+        "userId": "5b7c52fd60e2580cfeb26b24",
+        "name": "Neftlix",
+        "bills": [
+            {
+                "isPaid": false,
+                "_id": "5b7c6118598d8d0e674756a9",
+                "frequency": "monthly",
+                "dueDate": "2018-08-21T18:54:57.000Z",
+                "amount": 100
+            }
+        ],
+        "createdAt": "2018-08-21T18:59:36.918Z",
+        "updatedAt": "2018-08-21T18:59:36.918Z",
+        "id": "5b7c6118598d8d0e674756a8"
+    }
+]
+*/
