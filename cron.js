b3cdf26ce467cceffbc4f423b4b5cf75bddab401
Steven Imus <imussg@gmail.com>
2018-08-28 15:14:20 -0400

    work in progress

--- cron.js
+++ cron.js
@@ -1,15 +1,35 @@
 'use strict';
+const CronJob = require('cron').CronJob;
+const moment = require('moment');
 
-const { Account } = require('../models/accounts.js');
+const { Account } = require('./models/accounts.js');
 
-module.exports = {
-  cronJobRebatch: function() {
-    Account.find()
-      .then(accounts => {
-        // recreate all cron jobs based on the "reminder", "dueDate" fields
-      });
-  },
-  cronJobCreate: function(account) {
-    // create cronjob here
-  }
-};
+const buildCronTime = (aMoment) => {
+	let str = `${aMoment.seconds()} ${aMoment.minutes()} ${aMoment.hours()} ${aMoment.date()} * *`;
+	console.log(str);
+}
+
+const cronJobCreate = (account) => {
+	// create cronjob here
+    let count = 0;
+    let job;
+    // let timing = buildCronTime(account.bills[account.bills.length-1].dueDate);
+    console.log();
+    // while(count < 5) {
+	job = new CronJob("*/2 * * * * *", function() {
+    	count++;
+    	console.log(`Cron job tick #${count}! account name: ${/*account.name*/""}, time: ${moment()}`)
+    }, null, true);
+    job.start();
+}
+Account.find()
+	.then(accounts => console.log("printing something at least"));
+// console.log();
+// cronJobCreate(Account.find());
+// buildCronTime(moment());
+
+const cronJobRebatch = () => {
+
+}
+
+module.exports = {cronJobRebatch, cronJobCreate};
